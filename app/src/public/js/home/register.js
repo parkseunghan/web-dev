1b136a0c89902c957510bb7930b3de48bd36c9a0
parkseunghan <01seunghan@gmail.com>
2024-07-23 00:22:35 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,21 +1,28 @@
 'use strict';
 
 const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
     pw = document.querySelector("#pw"),
-    loginButton = document.querySelector("#button");
+    confirmPw = document.querySelector("#confirm-pw"),
+    registerButton = document.querySelector("#button");
 // #id 태그명과 id명을 구분하기 위해 선택자 앞에 #을 써줌
 // button은 선택자 그대로여서 button을 그대로 씀
 // 선택자: HTML태그 안에서 id="id", id="pw"같은 것
 
-loginButton.addEventListener("click", login);
+console.log("This is register");
 
-function login() {
+registerButton.addEventListener("click", register);
+
+function register() {
     const req = {
         id: id.value,
+        name: name.value,
         pw: pw.value,
+        confirmPw: confirmPw.value,
     };
+    
 
-    fetch("/login", {
+    fetch("/register", {
         method: "POST", // restapi관련
         headers: {
             "Content-Type": "application/json", // 전달하는 데이터가 JSON데이터라고 명시적으로 알려줌
@@ -25,12 +32,12 @@ function login() {
         .then((res) => res.json())
         .then((res) => {
             if (res.success) {
-                location.href = "/";
+                location.href = "/login";
             } else {
                 alert(res.msg);
             }
         }).catch((err) => {
-            console.error(new Error("로그인 중 에러가 발생했습니다."));
+            console.error(new Error("회원가입 중 에러가 발생했습니다."));
 
         })
 }
